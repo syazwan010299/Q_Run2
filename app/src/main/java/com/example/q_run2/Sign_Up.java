1b2995a6a1a2e02ace6ee0dae567ccaec582566b
syazwan010299 <41717306+syazwan010299@users.noreply.github.com>
2019-12-13 12:21:00 +0800

    update 1.10

--- app/src/main/java/com/example/q_run2/Sign_Up.java
+++ app/src/main/java/com/example/q_run2/Sign_Up.java
@@ -20,6 +20,6 @@ public class Sign_Up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up);
-        
+
     }
 }
